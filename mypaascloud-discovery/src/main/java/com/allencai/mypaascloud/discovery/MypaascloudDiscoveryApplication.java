0a69edc87e7cc8b9444aedc4e61a7b4dfc0fc526
caizl.hunter <caizl@hunter.dev>
2019-12-25 16:59:56 +0800

    Initial commit

---
+++ mypaascloud-discovery/src/main/java/com/allencai/mypaascloud/discovery/MypaascloudDiscoveryApplication.java
@@ -0,0 +1,17 @@
+package com.allencai.mypaascloud.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class MypaascloudDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypaascloudDiscoveryApplication.class, args);
+    }
+
+}
