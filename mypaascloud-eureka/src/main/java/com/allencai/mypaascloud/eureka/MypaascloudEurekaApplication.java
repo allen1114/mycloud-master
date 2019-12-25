2c1401d49dd114838cde03421b8224fd392a4750
caizl.hunter <caizl@hunter.dev>
2019-12-25 18:45:39 +0800

    eureka security csrf问题，config server , gateway

--- mypaascloud-eureka/src/main/java/com/allencai/mypaascloud/eureka/MypaascloudEurekaApplication.java
+++ mypaascloud-eureka/src/main/java/com/allencai/mypaascloud/eureka/MypaascloudEurekaApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -11,6 +14,15 @@ public class MypaascloudEurekaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MypaascloudEurekaApplication.class, args);
+    }
+
+    @EnableWebSecurity
+    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            super.configure(http);
+            http.csrf().ignoringAntMatchers("/eureka/**");
+        }
     }
 
 }
--- mypaascloud-gateway/src/test/java/com/allencai/mypaascloud/gateway/MypaascloudGatewayApplicationTests.java
+++ mypaascloud-gateway/src/test/java/com/allencai/mypaascloud/gateway/MypaascloudGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.allencai.mypaascloud.discovery;
+package com.allencai.mypaascloud.gateway;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MypaascloudDiscoveryApplicationTests {
+public class MypaascloudGatewayApplicationTests {
 
     @Test
     public void contextLoads() {
