bba4d3a1fd1be4d0c1dff57f04376ad1563b6ef3
caizl.hunter <caizl@hunter.dev>
2019-12-27 16:32:35 +0800

    调整命名，结构

---
+++ mycloud-infrastructure/mycloud-eureka/src/main/java/com/allencai/mycloud/eureka/Application.java
@@ -0,0 +1,27 @@
+package com.allencai.mycloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @EnableWebSecurity
+    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            super.configure(http);
+            http.csrf().ignoringAntMatchers("/eureka/**");
+        }
+    }
+
+}
